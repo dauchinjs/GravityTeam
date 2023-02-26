6416d1a4f78d5b343856aa3d78badb2b13681452
Davids Jansons <93677423+dauchinjs@users.noreply.github.com>
2023-02-26 15:50:55 +0200

    Update task4.php

--- GravityTeam/task4.php
+++ GravityTeam/task4.php
@@ -14,4 +14,6 @@ for ($i = $start; $i <= $end; $i++) {
 }
 
 echo "The sum of all digits of all natural numbers between 0 and 10^12 is: " . $sum . "\n"; // result is 73800000001
-
+//It's worth noting that the code runs without any issues until $end is set to 10^7, meaning
+//it computes the correct sum for natural numbers between 0 and 10^7.
+//However, if we set the $end value to a larger number like 10^12, the code may use a lot of memory and take a long time to execute.
